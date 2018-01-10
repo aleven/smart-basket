466879158a61401564d681175cefe318a3d8aeb5
Mirco <amirco@gmail.com>
2018-01-10 14:46:39 +0100

    rimosso pulsanti toolbar

--- basket-mockup-vuetify/src/App.vue
+++ basket-mockup-vuetify/src/App.vue
@@ -25,7 +25,7 @@
 
     <v-toolbar fixed app :clipped-left="clipped">
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
+      <!-- <v-btn icon @click.stop="miniVariant = !miniVariant">
         <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
       </v-btn>
       <v-btn icon @click.stop="clipped = !clipped">
@@ -33,7 +33,7 @@
       </v-btn>
       <v-btn icon @click.stop="fixed = !fixed">
         <v-icon>remove</v-icon>
-      </v-btn>
+      </v-btn> -->
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <!-- <v-spacer></v-spacer> -->
       <!-- <v-btn icon @click.stop="rightDrawer = !rightDrawer">
